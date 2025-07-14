f3217abec93a7990b91c23a8483589ac0eb9abd7
Thanakit <ss545370@gmail.com>
2025-07-14 14:35:02 +0700

    update

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json);
-app.use(express.urlencoded({extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API ");
