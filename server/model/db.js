a7f374ef1bbbee8b5729ab846fc7d0efba82dd37
Thanakit <ss5453702@gmail.com>
2025-09-01 14:19:50 +0700

    fix_db.js

--- server/model/db.js
+++ server/model/db.js
@@ -1,21 +1,27 @@
-import { Sequelize } from 'sequelize';
-import dbConfig from '../config/db.config.js';
+import { Sequelize } from "sequelize";
+import dbConfig from "../config/db.config.js";
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    port: dbConfig.PORT,
-    dialect: dbConfig.dialect,
-    logging: false,
+  host: dbConfig.HOST,
+  port: dbConfig.PORT,
+  dialect: dbConfig.dialect,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 testConnection();
 export default sequelize;
